3b3f083a66bab2499f0a89819c849dab353b1318
Sonia Baptista <sonia.baptista095@gmail.com>
2022-10-20 23:14:08 +0200

    add form

--- form.php
+++ form.php
@@ -11,26 +11,25 @@
 
 <body>
   <form action="thanks.php" method="post">
-
     <div>
       <label for="nom">Nom :</label>
-      <input type="text" id="nom" name="user_name">
+      <input type="text" id="nom" name="user_name" required>
     </div>
     <label for="prenom">Prenom :</label>
-    <input type="text" id="prenom" name="first_name">
+    <input type="text" id="prenom" name="first_name" required>
     </div>
     <div>
       <label for="courriel">Courriel :</label>
-      <input type="email" id="courriel" name="user_email">
+      <input type="email" id="courriel" name="user_email" required>
     </div>
     <div>
       <label for="phone">Tel :</label>
-      <input type="tel" id="phone" name="Phone_Number">
+      <input type="tel" id="phone" name="Phone_Number" required>
     </div>
     <div>
-      <select name="message" id="message">
-        <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
+
+      <label for="message">Message :</label>
+      <textarea id="message" name="user_message" required></textarea>
     </div>
     <div class="button">
       <button type="submit">Envoyer votre message</button>
@@ -40,9 +39,7 @@
         <option value="religions">Musique</options>
       </select>
     </div>
+
   </form>
-
-
-</body>
 
 </html>
--- thanks.php
+++ thanks.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 
+
 <body>
     <p> Merci
         <?= $user_name = $_POST['user_name'] ?>
@@ -19,6 +20,53 @@
         <?= $user_name = $_POST['user_name'] ?> ou par téléphone au
         <?= $phone_Number = $_POST['Phone_Number'] ?> dans les plus brefs délais pour traiter votre demande :
         <?= $message = $_POST['user_message'] ?>
+        /* Creating an array of errors. */
+        <?php $errors = [];
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // nettoyage et validation des données soumises via le formulaire 
+    if (!isset($_POST['user_name']) && trim($_POST['user_name']) === '')
+        $errors[] = "Le nom est obligatoire";
+
+    if (!isset($_POST['first_name']) && trim($_POST['first_name']) === '')
+        $errors[] = "Le nom est obligatoire";
+
+    if (!isset($_POST['user_email']) && trim($_POST['user_email']) && filter_var($_POST, FILTER_VALIDATE_EMAIL))
+        $errors[] = "L'adresse email est obligatoire";
+
+    if (!isset($_POST['Phone_Number']) && trim($_POST['Phone_Number']) === '')
+        $errors[] = "Le numero de telephone est obligatoire";
+
+    if (!isset($_POST['sujets']) && trim($_POST['sujets']) === '')
+        $errors[] = "Veuillez choisir un sujet ";
+
+    if (!isset($_POST['user_message']) & trim($_POST['user_message']) === '')
+        $errors[] = "Veuillez entrer un message ";
+
+
+    if (empty($errors)) {
+        // traitement du formulaire
+        // puis redirection
+        header('Location: thanks.php');
+    } else { ?>
+
+    </p>
+    <div class="border border-danger rounded p-3 m-5 bg-danger">
+        <ul>
+            <?php foreach ($errors as $error): ?>
+            <li>
+                <?= $error; ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+
+    </div>
+    <?php } ?>
+    <?php } ?>
+
+
+
 
 </body>
 
